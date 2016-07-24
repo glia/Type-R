5c471c79aab132bb4bfe061073d802475d2f5021
Vlad Balin <gaperton@gmail.com>
2016-07-23 20:18:03 -0400

    Added notes

--- src/record/nestedTypes.ts
+++ src/record/nestedTypes.ts
@@ -22,7 +22,51 @@ export class TransactionalType extends GenericAttribute {
     handleChange( next : Transactional, prev : Transactional, record : Record ){
         prev && free( record, prev );
         next && aquire( record, next, this.name );
+
+        if( next ){
+            const { name } = this;
+            if( next._owner ){
+                record.listenTo( next, 'change', function( child ){
+                    record._onChildrenChange( child, name );
+                });
+            }
+            else{
+                aquire( record, next, name );
+            }
+        }
     }
 }
 
 Record._attribute = TransactionalType;
+
+/**
+ * Two-phase commit is simple.
+ * 
+ * We always send two notifications.
+ * First one marks parent as dirty, incrementing its counter.
+ * It stops going upper, if parent is already dirty.
+ * 
+ * Second one performs an update, decrementing the counter.
+ * It stops going further if parent is still dirty.
+ * 
+ * Need to generalize this behavior for deep updates.
+ * 
+ * 
+ * !!!!!! Silent operates wrong. It must not prevent parent updates.
+ * 
+ * 
+ * !!!! Deep update can only work on owned objects. Only trees.
+ * Current algorithm is perfect for the owned trees. By all means.
+ * 
+ * 
+ * 
+ * We also have shared trees. (Can be handled with 'change' or 'commit' event)
+ * 
+ * And rombs. We cannot handle rombs.
+ * 
+ * In these cases, we need to bubble updates up. Can we use two phase for bubbling?
+ * Say, 'begin' event opens transaction everywhere, 'commit' event closes transaction.
+ * We really need to replace boolean flag with counter for that.
+ * 
+ * Looks like it will do the trick. But obviously, it will slow down the things a bit.
+ */
